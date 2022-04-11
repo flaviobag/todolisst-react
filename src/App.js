a7fe577a2aca01d5b673be852cd055ad50dd00e0
flaviobag <flaviobag@gmail.com>
2022-04-11 10:13:10 -0300

    adicionar header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <div className="main-card">
-
+        <Header/>
       </div>
     </div>
   );
