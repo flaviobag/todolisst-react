1168f4a1befd55a6af0d0c11f335a2cd4ff87efe
flaviobag <flaviobag@gmail.com>
2022-04-28 10:38:36 -0300

    adiciona cards

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,18 +3,13 @@ import './List.css';
 import Card from "../Card/Card";
 
 
-const List = () => {
-    const cards = [
-        {id: 1, title: "Qualquer coisa"},
-        {id: 2, title: "Outra Coisa"},
-        {id: 3, title: "Repete a outra coisa"}
-    ]
+const List = ({cards}) => {
+    
     return(
     <ul data-list className="cards">
         {
             cards.map((card) => (<Card id={card.id} title={card.title}/>))
         }
-        
     </ul>
 )}
 
