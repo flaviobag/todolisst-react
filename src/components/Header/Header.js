bc9aaba37f3fbbb609caf9f6170c835cce6873fc
flaviobag <flaviobag@gmail.com>
2022-05-09 10:47:59 -0300

    muda o botao de aparecer o input

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = ({open,setOpen}) => (
         <h1 data-title className="header__title">
         </h1>
         <h2 className="header__subtitle"><span data-greeting></span>You've got <span data-counter></span> things to do today!</h2>  
-        <Button type="button" onClick={() => setOpen(!open)}>+</Button>
+        <Button type="button" onClick={() => setOpen(!open)}>{open ? '-' : '+'}</Button>
     </header>
 )
 export default Header
